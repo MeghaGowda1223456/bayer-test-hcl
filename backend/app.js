2e656a5cee2297c03d387d5dd780707dc3f1fe70
Ragini R <ragini.ramashetty@ust.com>
2024-12-02 14:15:24 +0530

    commit api changes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var app = express();
+var dbcon = require("./dbcon/db");
+var user = require("./model/userHandler");
+var feature = require("./model/featureHandler");
+var dbconn = require("./dbcon/db");
 
 
+app.get("/feature", (req, res, next) => 
+{   
+    let sql = "select id,name,description from hosp_feature";
+    dbconn.query(sql,(err,rows) =>
+    {
+        if(err) throw err;
 
-app.get("/test", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+        res.send(rows);
+ 
+    });
+});
 
 
 app.listen(3000, () => {
